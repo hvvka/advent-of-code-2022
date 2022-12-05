c248f96de27fca7944be1c641389c56487394527
Hanna Grodzicka <hania.grodzicka@gmail.com>
2022-12-05 22:30:37 +0100

    Day 04

---
+++ src/main/kotlin/day04/PartOne.kt
@@ -0,0 +1,31 @@
+package day04
+
+import FileReader
+import java.lang.IllegalArgumentException
+
+fun main() {
+    val input = FileReader().readFile("day04/input.txt")
+
+    val fullyContainedAssignmentPairs = input.split("\n").map { line ->
+        val firstElfSection = line.split(",")[0]
+        val secondElfSection = line.split(",")[1]
+        doesOneFullyContainsOther(firstElfSection, secondElfSection)
+    }.count { result -> result }
+
+    println(fullyContainedAssignmentPairs) // 550
+}
+
+private fun doesOneFullyContainsOther(firstElfSection: String, secondElfSection: String): Boolean {
+    val firstElfSectionLow = firstElfSection.split("-")[0].toInt()
+    val firstElfSectionHigh = firstElfSection.split("-")[1].toInt()
+    val secondElfSectionLow = secondElfSection.split("-")[0].toInt()
+    val secondElfSectionHigh = secondElfSection.split("-")[1].toInt()
+
+    return if (firstElfSectionLow == secondElfSectionLow) {
+        true
+    } else if (firstElfSectionLow < secondElfSectionLow) {
+        firstElfSectionHigh >= secondElfSectionHigh
+    } else {
+        firstElfSectionHigh <= secondElfSectionHigh
+    }
+}
